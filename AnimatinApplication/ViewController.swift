d9ae2b7107cbf13305c4df1398bc13f8f8e9399d
Filin <nastia-dogf@mail.ru>
2022-07-29 19:08:16 +0300

    Added outlet and action

--- AnimatinApplication/ViewController.swift
+++ AnimatinApplication/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //MARK: - Outlets
+    @IBOutlet weak var coreAnimationVIew: UIView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+//MARK: - Actions
+    @IBAction func coreAnimationButtonTapped(_ sender: UIButton) {
+    }
+    
 
 }
 
