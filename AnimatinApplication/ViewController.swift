9a4bd6c01555a5cf0c0cd8cfdbe6ebd19b3f4547
Filin <nastia-dogf@mail.ru>
2022-08-01 12:59:24 +0300

    changed constraints

--- AnimatinApplication/ViewController.swift
+++ AnimatinApplication/ViewController.swift
@@ -10,16 +10,28 @@ import UIKit
 class ViewController: UIViewController {
     
     //MARK: - Outlets
-    @IBOutlet weak var coreAnimationVIew: UIView!
+    @IBOutlet var coreAnimationView: UIView!
     
+    private var originCoordinate: CGFloat?
 
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
     }
     
 //MARK: - Actions
     @IBAction func coreAnimationButtonTapped(_ sender: UIButton) {
+       // sender.pulsate()
+        
+        print(coreAnimationView.frame.origin.x)
+        self.coreAnimationView.frame.origin.x -= 20
+    
+        print(coreAnimationView.frame.origin.x)
+      
     }
     
 
